1badbfb3285a159fb2567358d683f7bddcd51fef
Huda Ramadan <011hodaramadan@gmail.com>
2025-11-03 05:52:30 -0800

    theme dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nodes_app/views/home_page.dart';
 
 void main() {
   runApp(Nodes_app());
@@ -9,6 +10,10 @@ class Nodes_app extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
