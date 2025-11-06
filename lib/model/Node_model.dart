aeac4f0fd77efc525adfd02f75cde6d40f47e02d
Huda Ramadan <011hodaramadan@gmail.com>
2025-11-06 14:17:35 -0800

    create node model

---
+++ lib/model/Node_model.dart
@@ -0,0 +1,13 @@
+class NodeModel {
+  final String title;
+  final String subtitle;
+  final String data;
+  final int color;
+
+  NodeModel({
+    required this.title,
+    required this.subtitle,
+    required this.data,
+    required this.color,
+  });
+}
