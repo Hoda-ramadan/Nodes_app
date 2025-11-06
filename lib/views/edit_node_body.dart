69a29797e640c450eb2c3d3cc250e0c3f329bd61
Huda Ramadan <011hodaramadan@gmail.com>
2025-11-06 13:32:14 -0800

    create edit node view

---
+++ lib/views/edit_node_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:nodes_app/widgets/edit_node_body_view.dart';
+
+class EditNodeBody extends StatelessWidget {
+  const EditNodeBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: EditNodeBodyView());
+  }
+}
